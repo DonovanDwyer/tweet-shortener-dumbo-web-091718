6edf6607f6a8fe73639f776a30622046d23bb6e1
Donovan Dwyer <dwyer.donovan@gmail.com>
2018-09-11 02:50:04 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+require "pry"
+
+def dictionary
+  dictionary = {
+    :hello => "hi",
+    :to => "2",
+    :two => "2",
+    :too => "2",
+    :for => "4",
+    :four => "4",
+    :be => "b",
+    :you => "u",
+    :at => "@",
+    :and => "&"
+  }
+end
+
+def word_substituter(tweet_str)
+  tweet = tweet_str.split(" ")
+  shortened = []
+  tweet.map do |word|
+    dictionary.map do |key, value|
+      if word.to_s == key.to_s
+        shortened << value.to_s
+      else
+        shortened << word.to_s
+      end
+    end
+  end
+  shortened.join(" ")
+end
+
+#pry.binding
